7de78af33613fc00ae58de8b0fca8f3f985c896e
chunkyasparagus <icanseethepubfromhere@gmail.com>
2019-05-25 18:42:02 +0900

    Added new grid template Cannot load the static css file for some reason.

--- gridview/views.py
+++ gridview/views.py
@@ -2,23 +2,25 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Person
 
+from itertools import count
+
 
 def viewgrid(request):
+    datan = count(1)
     context = dict()
     context['rows'] = list()
     new_row = list()
-    value = {'value': 'XO'}
-    new_row.append(value)
+    new_row.append({'class': 'tl', 'isdata': False, 'label': 'XO'})
     everyone = Person.objects.all()
     for bondee in everyone:
-        new_row.append({'class': 'topheader', 'isdata': False, 'value': bondee.name})
+        new_row.append({'class': 'th', 'isdata': False, 'label': bondee.name})
     context['rows'].append(new_row)
 
     for bonder in everyone:
         new_row = list()
-        new_row.append({'class': 'leftheader', 'isdata': False, 'value': bonder.name})
+        new_row.append({'class': 'lh', 'isdata': False, 'label': bonder.name})
         for bondee in everyone:
-            new_row.append({'class': 'gridvalue', 'isdata': True, 'value': '0'})
+            new_row.append({'class': 'c', 'isdata': True, 'datavalue': '0', 'dataname': f'griddata{next(datan)}'})
         context['rows'].append(new_row)
 
     return render(request, 'grid.html', context)
