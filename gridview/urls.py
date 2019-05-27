2ad754c34580b83ad4a5a676606bc81e6599e70a
chunkyasparagus <icanseethepubfromhere@gmail.com>
2019-05-27 19:31:30 +0900

    Added correct values from Bond objects to the grid Created a view to save the grid values

--- gridview/urls.py
+++ gridview/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
 from .views import viewgrid, updategrid
-
 urlpatterns = [
     path('', viewgrid, name='viewgrid'),
     path('updategrid', updategrid, name='updategrid'),
--- gridview/views.py
+++ gridview/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from .models import Person
+from django.core.exceptions import ObjectDoesNotExist
+from .models import Person, Bond
 
 from itertools import count
+from collections import OrderedDict
 
 
 def viewgrid(request):
@@ -20,11 +22,28 @@ def viewgrid(request):
         new_row = list()
         new_row.append({'class': 'lh', 'isdata': False, 'label': bonder.name})
         for bondee in everyone:
-            new_row.append({'class': 'c', 'isdata': True, 'datavalue': '0', 'dataname': f'griddata{next(datan)}'})
+            try:
+                v = Bond.objects.get(bonder__id=bonder.id, bondee__id=bondee.id).bond_level
+            except ObjectDoesNotExist:
+                new_object = Bond(bonder=bonder, bondee=bondee)
+                new_object.save()
+                v = new_object.bond_level
+            new_row.append({'class': 'c', 'isdata': True, 'datavalue': f'{v}', 'dataname': f'griddata_{next(datan)}'})
         context['rows'].append(new_row)
 
     return render(request, 'grid.html', context)
 
 
 def updategrid(request):
-    return HttpResponse('success')
+    raw_grid_data = {int(key.split('_')[1]): data for key, data in request.POST.items() if key.startswith('griddata_')}
+    grid_data = list(OrderedDict(sorted(raw_grid_data.items())).values())
+    everyone = Person.objects.all()
+    for bonder in everyone:
+        for bondee in everyone:
+            v = grid_data.pop(0)
+            bond_obj = Bond.objects.get(bonder__id=bonder.id, bondee__id=bondee.id)
+            if v != bond_obj.bond_level:
+                bond_obj.bond_level = v
+                bond_obj.save()
+
+    return redirect('viewgrid')
